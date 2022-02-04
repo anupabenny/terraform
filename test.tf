23eee7016d25c9b1547d00aeafc893524c52322f
anupabenny <81522640+anupabenny@users.noreply.github.com>
2022-02-04 12:58:26 +0000

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,13 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = data.aws_ssm_parameter.access_key
-  secret_key = data.aws_ssm_arameter.access_key
-}
-data "aws_ssm_paramater" "access_key" {
-  name = "access_key"
-}
-data "aws_ssm_paramater" "secret_key" {
-  name = "secret_key"
 }
 resource "aws_iam_user" "lb" {
   name = "demo"
